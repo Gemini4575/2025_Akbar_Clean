bd800c5837a7dd74b5881cbfd141d7281278a0de
Andrey Utis <andrey81inmd@gmail.com>
2025-09-07 22:16:34 -0400

    test

--- src/test/java/robot/PoseEstimationTest.java
+++ src/test/java/robot/PoseEstimationTest.java
@@ -17,51 +17,51 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
 public class PoseEstimationTest {
 
-    @Test
-    public void test() {
-        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_backLeftLocation,
-                m_backRightLocation,
-                m_frontRightLocation, m_frontLeftLocation);
+        @Test
+        public void test() {
+                SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_backLeftLocation,
+                                m_backRightLocation,
+                                m_frontRightLocation, m_frontLeftLocation);
 
-        var startTime = System.currentTimeMillis() / 1000;
+                var startTime = System.currentTimeMillis() / 1000;
 
-        var stateStdDevs = VecBuilder.fill(0.1, 0.1, 0.1);
-        var visionStdDevs = VecBuilder.fill(1, 1, 1);
+                var stateStdDevs = VecBuilder.fill(0.1, 0.1, 0.1);
+                var visionStdDevs = VecBuilder.fill(1, 1, 1);
 
-        var initialGyroPose = Rotation2d.fromDegrees(0);
+                var initialGyroPose = Rotation2d.fromDegrees(0);
 
-        var initialModulePositions = new SwerveModulePosition[] {
-                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0))
-        };
+                var initialModulePositions = new SwerveModulePosition[] {
+                                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(0.0, Rotation2d.fromDegrees(0))
+                };
 
-        var poseEstimator = new SwerveDrivePoseEstimator(
-                m_kinematics,
-                initialGyroPose,
-                initialModulePositions,
-                new Pose2d(),
-                stateStdDevs,
-                visionStdDevs);
+                var poseEstimator = new SwerveDrivePoseEstimator(
+                                m_kinematics,
+                                initialGyroPose,
+                                initialModulePositions,
+                                new Pose2d(),
+                                stateStdDevs,
+                                visionStdDevs);
 
-        poseEstimator.resetPosition(Rotation2d.fromDegrees(0), initialModulePositions,
-                new Pose2d(1, 1, Rotation2d.fromDegrees(0)));
+                poseEstimator.resetPosition(Rotation2d.fromDegrees(0), initialModulePositions,
+                                new Pose2d(1, 1, Rotation2d.fromDegrees(-90)));
 
-        System.out.println("Initial pose: " + poseEstimator.getEstimatedPosition());
+                System.out.println("Initial pose: " + poseEstimator.getEstimatedPosition());
 
-        var modulePosition2 = new SwerveModulePosition[] {
-                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
-                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0))
-        };
+                var modulePosition2 = new SwerveModulePosition[] {
+                                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0)),
+                                new SwerveModulePosition(1.0, Rotation2d.fromDegrees(0))
+                };
 
-        var gyroPose2 = Rotation2d.fromDegrees(0);
+                var gyroPose2 = Rotation2d.fromDegrees(0);
 
-        poseEstimator.updateWithTime(startTime + 1, gyroPose2, modulePosition2);
+                poseEstimator.updateWithTime(startTime + 1, gyroPose2, modulePosition2);
 
-        System.out.println("Pose2: " + poseEstimator.getEstimatedPosition());
-    }
+                System.out.println("Pose2: " + poseEstimator.getEstimatedPosition());
+        }
 
 }
